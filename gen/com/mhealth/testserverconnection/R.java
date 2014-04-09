ba70160d68f2bb02c643da778f0fd6c63b406518
Yuxuan "Ethan" Chen <ychen18@nd.edu>
2014-04-09 11:38:16 -0400

    Successfully enabled writing to and reading from files in the internal storage.

--- gen/com/mhealth/testserverconnection/R.java
+++ gen/com/mhealth/testserverconnection/R.java
@@ -24,14 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TextView01=0x7f080004;
-        public static final int TextView02=0x7f080005;
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080004;
+        public static final int button_save=0x7f080001;
         public static final int button_update=0x7f080002;
-        public static final int editText_acceleration_x=0x7f080001;
-        public static final int editText_acceleration_y=0x7f080000;
-        public static final int editText_acceleration_z=0x7f080006;
-        public static final int textView1=0x7f080003;
+        public static final int editText_file=0x7f080000;
+        public static final int textView_status=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -43,6 +40,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int output_filename=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/mhealth/testserverconnection/MainActivity.java
+++ src/com/mhealth/testserverconnection/MainActivity.java
@@ -1,5 +1,12 @@
 package com.mhealth.testserverconnection;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
@@ -14,10 +21,13 @@ import com.mhealth.testserverconnection.R;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -26,16 +36,65 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		/* Event listener for the update button */
-		Button updateButton = (Button) findViewById(R.id.button_update);
-		updateButton.setOnClickListener(new View.OnClickListener() {
+		// Initalize controls of different layout items
+		final TextView fileView   = (TextView) findViewById(R.id.editText_file);
+		final TextView statusView = (TextView) findViewById(R.id.textView_status);
+		Button   saveButton     = (Button)   findViewById(R.id.button_save);
+		Button updateButton     = (Button)   findViewById(R.id.button_update);
+		
+		// Define the name of the file
+		final String fileName = getString(R.string.output_filename);
+					
+		// Event listener for the save button
+		saveButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				
-				DataStreamToServer serverConnection = new DataStreamToServer();
-				serverConnection.execute();
+				// Test writing and saving a file
+				String fileContent = fileView.getText().toString();
+				
+				try {
+				    FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+				    fos.write(fileContent.getBytes());
+				    fos.close();
+				    statusView.setText("Created the file.");
+				} catch (Exception e) {
+				    e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		// Event listener for the update button 
+		updateButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// Test reading and logging a file
+				BufferedReader reader;
+				try {
+					reader = new BufferedReader(new InputStreamReader(openFileInput(fileName)));
+				    StringBuilder sb = new StringBuilder();
+				    String line = null;
+					while ((line = reader.readLine()) != null) {
+						  sb.append(line).append("\n");
+					}
+				    reader.close();
+				    fileView.setText(sb.toString());
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			    
+				
+				//DataStreamToServer serverConnection = new DataStreamToServer();
+				//serverConnection.execute();
 			}
 		});
 		
@@ -55,7 +114,10 @@ public class MainActivity extends Activity {
 		protected HttpResponse doInBackground(String... params) {
 			// TODO Auto-generated method stub
 			
-			/* Get user input */
+			/*	
+			// Get user input
+			
+			
 			EditText blank_acceleration_x = (EditText) findViewById(R.id.editText_acceleration_x);
 			EditText blank_acceleration_y = (EditText) findViewById(R.id.editText_acceleration_y);
 			EditText blank_acceleration_z = (EditText) findViewById(R.id.editText_acceleration_z);
@@ -63,11 +125,11 @@ public class MainActivity extends Activity {
 			String acceleration_y = blank_acceleration_y.getText().toString();
 			String acceleration_z = blank_acceleration_z.getText().toString();
 			
-			/* Call the PHP script on the server */
+			// Call the PHP script on the server 
 			HttpClient httpClient = new DefaultHttpClient();
 			HttpPost          httpPost   = new HttpPost("http://mhealth1.cse.nd.edu/tests/AndroidInsertData.php");
 		
-			/* Post the input data */
+			// Post the input data 
 			try {
 				ArrayList<NameValuePair> person = new ArrayList<NameValuePair>();
 				person.add(new BasicNameValuePair("AccelerationX", acceleration_x));
@@ -81,7 +143,7 @@ public class MainActivity extends Activity {
 			catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+			*/
 			return null;
 		}
 		
